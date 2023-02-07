9a50acd3d5fa1e09ba60d4ff259e9971f3ca2f04
Ankit Banerjee <63962369+ankitbanerjee0211@users.noreply.github.com>
2023-02-07 21:53:14 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
-
+    class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+    
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        
+        public double product(double x, double y) {
+            double pro = x*y;
+            return pro;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(2, 5));
+        System.out.println(p.product(2, 5, 10));
+        System.out.println(p.product(1.2, 5.5));
+    }
 }
